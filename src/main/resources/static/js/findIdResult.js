b68a5fcf16786abb3291d5ab6a943606de8d06fa
ph0109 <pf6350@gmail.com>
2024-06-03 20:54:03 +0900

    findIdResult.js 코드 수정

--- src/main/resources/static/js/findIdResult.js
+++ src/main/resources/static/js/findIdResult.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // AJAX 요청으로 서버에서 아이디를 받아옵니다.
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/user/findIdResult', true); // '/user/getFoundId'는 서버에서 아이디를 반환하는 엔드포인트로 가정합니다.
+    xhr.open('GET', '/user/findIdResult', true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var foundId = xhr.responseText.trim();
@@ -11,6 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
     xhr.send();
 
     document.getElementById('backToLoginButton').addEventListener('click', function() {
-        window.location.href = 'login.html'; // 로그인 페이지로의 경로를 설정합니다.
+        window.location.href = 'login.html';
     });
 });
